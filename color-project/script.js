b93cb8f479538300823a090600cc5c21c5be2e5e
puduru soujanya <vtu26112@veltech.edu.in>
2026-04-15 10:20:52 +0530

    folder added

---
+++ color-project/script.js
@@ -0,0 +1,7 @@
+function changeTextColor(color) {
+    document.getElementById("text").style.color = color;
+}
+
+function changeBgColor(color) {
+    document.getElementById("box").style.background = color;
+}
